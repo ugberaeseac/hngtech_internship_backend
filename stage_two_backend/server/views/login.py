b27e648bd1d73ecbff07b27c919648dce5f2156b
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-07-06 08:57:06 -0400

    Implemented the view API endpoint to authenticate and Log in an authorized user

---
+++ stage_two_backend/server/views/login.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+API endpoint route to authenticate a user
+Authenticates a user and creates a JWT token
+if successful, returns the user id and access token else
+displays an incorrect username/password message
+"""
+
+from server import db, bcrypt
+from server.models import User
+from server.views import app_views
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token
+
+
+@app_views('/auth/login', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    authenticates and log in an authorized user
+    """
+    data - request.get_json()
+    errors = []
+
+    email = data['email']
+    if not email:
+        errors.append({'field': 'email', 'message': 'Please enter an email address'})
+    password = data['password']
+    if not password:
+        errors.append({'field': 'password', 'message': 'Please enter a password'})
+
+    if errors:
+        return jsonify({'errors': errors}), 422
+
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity=user.userId)
+    	return jsonify({
+            'status': 'success',
+            'message': 'Login successful',
+            'data': {
+                'accessToken': access_token,
+                'user': {
+                    'userId': user.userId,
+                    'firstName': user.firstName,
+                    'lastName': user.lastName,
+                    'email': user.email,
+                    'phone': user.phone,
+                    }
+                }
+            }), 201
+    else:
+        return jsonify({
+            'status': 'Bad request',
+            'message': 'Authentication failed',
+            'statusCode': 401
+            }), 401
+
