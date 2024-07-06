d99272f94e67f287da2bdec79bb0f3a75bf57f98
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2024-07-06 13:56:28 -0400

    DEBUG: Fixed endpoints typo error

---
+++ stage_two_backend/server/views/__init__.py
@@ -0,0 +1,14 @@
+#!?usr/bin/python3
+"""
+Create a Blueprint
+Import views for the endpoints
+"""
+
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api')
+
+
+from server.views.register import *
+from server.views.login import *
